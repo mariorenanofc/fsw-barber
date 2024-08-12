21b122453e669bdf282d3737addcd8c36e601dae
mariorenanofc <mariorenan25@gmail.com>
2024-08-12 16:08:17 -0300

    feat: add Header  component and Banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,34 @@
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/header"
+import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
+
 const Home = () => {
-  return ( 
-    <h1 className="w-full text-yellow-300 mb-5 bg-red-400">Home page</h1>
+  return (
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Mário Renan</h2>
+        <p>Segunda Feira, 12 de Agosto, de 2024</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+            alt="Agente nos melhores com FSW Barber"
+          />
+        </div>
+      </div>
+    </div>
   )
 }
- 
-export default Home;
+
+export default Home
